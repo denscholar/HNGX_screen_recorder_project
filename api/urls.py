da7b5ff007099de414262922f9a8a58810ccb15e
Dennis Akagha <d.akagha20@gmail.com>
2023-09-30 13:53:42 +0100

    refactor code and add swagger

--- api/urls.py
+++ api/urls.py
@@ -28,5 +28,5 @@ urlpatterns = [
     re_path(
         r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
-    path("", include("videos.urls")),
+    path("videos/", include("videos.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
